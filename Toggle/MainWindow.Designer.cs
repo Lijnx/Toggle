f90f1d4aadda9d80821f7c76b6b75652cf19afd6
Lijnx <lynx20030311@gmail.com>
2022-02-21 19:48:26 +0700

    Добавьте файлы проекта.

---
+++ Toggle/MainWindow.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Toggle
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.BassTrackBar = new System.Windows.Forms.TrackBar();
+            this.MiddleTrackBar = new System.Windows.Forms.TrackBar();
+            this.TrebleTrackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BassPitchIntencity = new System.Windows.Forms.Label();
+            this.MiddlePitchIntencity = new System.Windows.Forms.Label();
+            this.TreblePitchIntencity = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.BassTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MiddleTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrebleTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartBtn.Location = new System.Drawing.Point(455, 40);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 63);
+            this.StartBtn.TabIndex = 0;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Enabled = false;
+            this.StopBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StopBtn.Location = new System.Drawing.Point(455, 120);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(75, 63);
+            this.StopBtn.TabIndex = 1;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // BassTrackBar
+            // 
+            this.BassTrackBar.Location = new System.Drawing.Point(69, 40);
+            this.BassTrackBar.Maximum = 20;
+            this.BassTrackBar.Minimum = 5;
+            this.BassTrackBar.Name = "BassTrackBar";
+            this.BassTrackBar.Size = new System.Drawing.Size(380, 45);
+            this.BassTrackBar.TabIndex = 2;
+            this.BassTrackBar.Value = 10;
+            this.BassTrackBar.Scroll += new System.EventHandler(this.Bass_Scroll);
+            // 
+            // MiddleTrackBar
+            // 
+            this.MiddleTrackBar.Location = new System.Drawing.Point(69, 91);
+            this.MiddleTrackBar.Maximum = 20;
+            this.MiddleTrackBar.Minimum = 5;
+            this.MiddleTrackBar.Name = "MiddleTrackBar";
+            this.MiddleTrackBar.Size = new System.Drawing.Size(380, 45);
+            this.MiddleTrackBar.TabIndex = 3;
+            this.MiddleTrackBar.Value = 10;
+            this.MiddleTrackBar.Scroll += new System.EventHandler(this.Middle_Scroll);
+            // 
+            // TrebleTrackBar
+            // 
+            this.TrebleTrackBar.Location = new System.Drawing.Point(69, 142);
+            this.TrebleTrackBar.Maximum = 20;
+            this.TrebleTrackBar.Minimum = 5;
+            this.TrebleTrackBar.Name = "TrebleTrackBar";
+            this.TrebleTrackBar.Size = new System.Drawing.Size(380, 45);
+            this.TrebleTrackBar.TabIndex = 4;
+            this.TrebleTrackBar.Value = 10;
+            this.TrebleTrackBar.Scroll += new System.EventHandler(this.Treble_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(69, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(380, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Pitch Effect";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(9, 40);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 46);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Bass";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(9, 91);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 46);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Middle";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(9, 142);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 46);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Treble";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BassPitchIntencity
+            // 
+            this.BassPitchIntencity.Location = new System.Drawing.Point(73, 71);
+            this.BassPitchIntencity.Name = "BassPitchIntencity";
+            this.BassPitchIntencity.Size = new System.Drawing.Size(376, 15);
+            this.BassPitchIntencity.TabIndex = 9;
+            this.BassPitchIntencity.Text = "1";
+            this.BassPitchIntencity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MiddlePitchIntencity
+            // 
+            this.MiddlePitchIntencity.Location = new System.Drawing.Point(73, 121);
+            this.MiddlePitchIntencity.Name = "MiddlePitchIntencity";
+            this.MiddlePitchIntencity.Size = new System.Drawing.Size(376, 15);
+            this.MiddlePitchIntencity.TabIndex = 10;
+            this.MiddlePitchIntencity.Text = "1";
+            this.MiddlePitchIntencity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TreblePitchIntencity
+            // 
+            this.TreblePitchIntencity.Location = new System.Drawing.Point(73, 168);
+            this.TreblePitchIntencity.Name = "TreblePitchIntencity";
+            this.TreblePitchIntencity.Size = new System.Drawing.Size(376, 15);
+            this.TreblePitchIntencity.TabIndex = 11;
+            this.TreblePitchIntencity.Text = "1";
+            this.TreblePitchIntencity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 195);
+            this.Controls.Add(this.TreblePitchIntencity);
+            this.Controls.Add(this.MiddlePitchIntencity);
+            this.Controls.Add(this.BassPitchIntencity);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TrebleTrackBar);
+            this.Controls.Add(this.MiddleTrackBar);
+            this.Controls.Add(this.BassTrackBar);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.StartBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "Toggle";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.BassTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MiddleTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrebleTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.TrackBar BassTrackBar;
+        private System.Windows.Forms.TrackBar MiddleTrackBar;
+        private System.Windows.Forms.TrackBar TrebleTrackBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label BassPitchIntencity;
+        private System.Windows.Forms.Label MiddlePitchIntencity;
+        private System.Windows.Forms.Label TreblePitchIntencity;
+    }
+}
+
---
+++ Toggle/MainWindow.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSCore;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+using CSCore.SoundOut;
+using CSCore.Streams;
+using CSCore.Streams.Effects;
+using CSCore.DSP;
+
+namespace Toggle
+{
+    public partial class MainWindow : Form
+    {
+        private readonly WasapiCapture soundIn;
+        private readonly WasapiOut soundOut;
+        private readonly PitchShifter pitchBass = null, pitchMiddle = null, pitchTreble = null;
+        private readonly DmoWavesReverbEffect reverbBass = null, reverbMiddle = null, reverbTreble = null;
+        //General variables
+        private const int SampleRate = 16000;
+        private const int GeneralBF = 65, GeneralTF = 7500;
+        //PitchEffect variables
+        private const int PitchBassBF = 300, PitchBassTF = 700;
+        private const int PitchMiddleBF = 1000, PitchMiddleTF = 2000;
+        private const int PitchTrebleBF = 2500, PitchTrebleTF = 3500;
+        //Reverb variables
+        private const int ReverbBassBF = 65, ReverbBassTF = 300;
+        private const int ReverbMiddleBF = 700, ReverbMiddleTF = 1100;
+        private const int ReverbTrebleBF = 2000, ReverbTrebleTF = 3000;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Initialize WasapiCapture for recording
+            soundIn = new WasapiCapture(true, AudioClientShareMode.Shared, 0);
+            soundIn.Initialize();
+
+            //Initialize the mixer
+            var mixer = new SimpleMixer(2, SampleRate)
+            {
+                FillWithZeros = true,
+                DivideResult = true
+            };
+
+            //Split the input signal into different frequency ranges, apply pitch and add to the mixer
+            if (GeneralBF != PitchBassBF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, GeneralBF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, PitchBassBF);
+
+                mixer.AddSource(nonEffectSource);
+            }
+
+            if (PitchBassBF != PitchBassTF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, PitchBassBF);
+                var pitchBassSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                pitchBassSource.Filter = new LowpassFilter(SampleRate, PitchBassTF);
+
+                pitchBass = new PitchShifter(pitchBassSource);
+                mixer.AddSource(pitchBass);
+            }
+
+            if (PitchBassTF != PitchMiddleBF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, PitchBassTF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, PitchMiddleBF);
+
+                mixer.AddSource(nonEffectSource);
+            }
+
+            if (PitchMiddleBF != PitchMiddleTF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, PitchMiddleBF);
+                var pitchMiddleSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                pitchMiddleSource.Filter = new LowpassFilter(SampleRate, PitchMiddleTF);
+
+                pitchMiddle = new PitchShifter(pitchMiddleSource);
+                mixer.AddSource(pitchMiddle);
+            }
+
+            if (PitchMiddleTF != PitchTrebleBF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, PitchMiddleTF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, PitchTrebleBF);
+
+                mixer.AddSource(nonEffectSource);
+            }
+
+            if (PitchTrebleBF != PitchTrebleTF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, PitchTrebleBF);
+                var pitchTrebleSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                pitchTrebleSource.Filter = new LowpassFilter(SampleRate, PitchTrebleTF);
+
+                pitchTreble = new PitchShifter(pitchTrebleSource);
+                mixer.AddSource(pitchTreble);
+            }
+
+            if (PitchTrebleTF != GeneralTF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, PitchTrebleTF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, GeneralTF);
+
+                mixer.AddSource(nonEffectSource);
+            }
+            /*
+            //Split the mixer signal into different frequency ranges, apply reverb and add to another mixer
+            if (GeneralBF != ReverbBassBF)
+            {
+                var source = new SimpleMixer(2, SampleRate);
+                source.AddSource(mixer);
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, GeneralBF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, ReverbBassBF);
+
+                mixer.AddSource(nonEffectSource);
+            }
+
+            if (ReverbBassBF != ReverbBassTF)
+            {
+                var source = new SimpleMixer(2, SampleRate);
+                source.AddSource(mixer);
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbBassBF);
+                var reverbBassSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                reverbBassSource.Filter = new LowpassFilter(SampleRate, ReverbBassTF);
+
+                reverbBass = new DmoWavesReverbEffect(reverbBassSource.ToWaveSource());
+                mixer.AddSource(reverbBass.ToSampleSource());
+            }
+
+            if (ReverbBassTF != ReverbMiddleBF)
+            {
+                var source = new SimpleMixer(2, SampleRate);
+                source.AddSource(mixer);
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbBassTF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, ReverbMiddleBF);
+
+                mixer.AddSource(nonEffectSource);
+            }
+
+            if (ReverbMiddleBF != ReverbMiddleTF)
+            {
+                var source = new SimpleMixer(2, SampleRate);
+                source.AddSource(mixer);
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbMiddleBF);
+                var reverbMiddleSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                reverbMiddleSource.Filter = new LowpassFilter(SampleRate, ReverbMiddleTF);
+
+                reverbMiddle = new DmoWavesReverbEffect(reverbMiddleSource.ToWaveSource());
+                mixer.AddSource(reverbMiddle.ToSampleSource());
+            }
+
+            if (ReverbMiddleTF != ReverbTrebleBF)
+            {
+                var source = new SimpleMixer(2, SampleRate);
+                source.AddSource(mixer);
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbMiddleTF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, ReverbTrebleBF);
+
+                mixer.AddSource(nonEffectSource);
+            }
+
+            if (ReverbTrebleBF != ReverbTrebleTF)
+            {
+                var source = new SimpleMixer(2, SampleRate);
+                source.AddSource(mixer);
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbTrebleBF);
+                var reverbTrebleSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                reverbTrebleSource.Filter = new LowpassFilter(SampleRate, ReverbTrebleTF);
+
+                reverbTreble = new DmoWavesReverbEffect(reverbTrebleSource.ToWaveSource());
+                mixer.AddSource(reverbTreble.ToSampleSource());
+            }
+
+            if (ReverbTrebleTF != GeneralTF)
+            {
+                var source = new SimpleMixer(2, SampleRate);
+                source.AddSource(mixer);
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbTrebleTF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, GeneralTF);
+
+                mixer.AddSource(nonEffectSource);
+            }
+            */
+            //Initialize the soundout with the final mixer 
+            soundOut = new WasapiOut() { Latency = 100 };
+            soundOut.Initialize(mixer.ToWaveSource());
+        }
+        
+        private void Bass_Scroll(object sender, EventArgs e)
+        {
+            if (pitchBass != null)
+            {
+                float value = (float) BassTrackBar.Value / 10;
+                pitchBass.PitchShiftFactor = value;
+                BassPitchIntencity.Text = value.ToString();
+            }
+                
+        }
+
+        private void Middle_Scroll(object sender, EventArgs e)
+        {
+            if (pitchMiddle != null)
+            {
+                float value = (float) MiddleTrackBar.Value / 10;
+                pitchMiddle.PitchShiftFactor = (float)MiddleTrackBar.Value / 10;
+                MiddlePitchIntencity.Text = value.ToString();
+            }
+        }
+
+        private void Treble_Scroll(object sender, EventArgs e)
+        {
+            if (pitchTreble != null)
+            {
+                float value = (float) TrebleTrackBar.Value / 10;
+                pitchTreble.PitchShiftFactor = (float)TrebleTrackBar.Value / 10;
+                TreblePitchIntencity.Text = value.ToString();
+            }
+        }
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            soundOut.Dispose();
+            soundIn.Dispose();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            soundIn.Start();
+            soundOut.Play();
+            StartBtn.Enabled = false;
+            StopBtn.Enabled = true;
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            soundIn.Stop();
+            soundOut.Stop();
+            StartBtn.Enabled = true;
+            StopBtn.Enabled = false;
+        }
+    }
+}
